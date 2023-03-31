43e4d715dba08af777c9f29a4d51eb3e26e1bb8b
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-03-30 18:37:53 -0600

    [Install]
    Instalacion de next

--- grupo-4/next.config.js
+++ grupo-4/next.config.js
@@ -2,5 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  serverOptions: {
+    host: '192.168.0.6'
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
+  babel: {
+    presets: ['next/babel'],
+  },
+};
