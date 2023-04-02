32e466292b60ad627d16768c0a2010cf19c02783
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-04-02 17:10:11 -0600

    [INSTALL TAILWIND]

--- grupo-4/src/components/pokemon/PokemonPrincipalCard.tsx
+++ grupo-4/src/components/pokemon/PokemonPrincipalCard.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { NextPage } from "next";
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
 import { Layout } from "../layouts";
 import { Pokemon } from "../../interfaces";
-import Image from "next/image";
 
 interface PokemonProps {
   pokemon: Pokemon;
@@ -11,29 +11,21 @@ interface PokemonProps {
 export const PokemonPrincipalCard: NextPage<PokemonProps> = ({ pokemon }) => {
   return (
     <Layout title={`${pokemon.name}`} data-testid={`pokemon-name`}>
-      <div>
+      <div className="flex flex-col bg-red-500">
         <div>
-          <div>
-            <main>
-              <Image src={pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name} />
-            </main>
-          </div>
+          <img src={pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name} />
         </div>
-        <div>
-          <div>
-            <header>
-              <h1>{pokemon.name}</h1>
-            </header>
 
-            <main>
-              <h2>Sprites:</h2>
-              <div>
-                <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
-              </div>
-            </main>
+        <div>
+          <h1>{pokemon.name}</h1>
+          <div>
+            <h2>Sprites:</h2>
+            <div>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
+              <img src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
+              <img src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
+              <img src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
+            </div>
           </div>
         </div>
       </div>
@@ -43,28 +35,31 @@ export const PokemonPrincipalCard: NextPage<PokemonProps> = ({ pokemon }) => {
 
 /*
 
-<Layout title={`${pokemon.name}`} data-testid={`pokemon-name`}>
-      <div>
+ <Layout title={`${pokemon.name}`} data-testid={`pokemon-name`}>
+      <div >
         <div>
           <div>
             <main>
-              <Image src={pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name} />
-            </main>
+            <img src= {pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name}  />
+             </main>
           </div>
         </div>
+
         <div>
           <div>
-            <header>
-              <h1>{pokemon.name}</h1>
+            <header >
+              <h1>
+                {pokemon.name}
+              </h1>
             </header>
 
             <main>
               <h2>Sprites:</h2>
               <div>
-                <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
               </div>
             </main>
           </div>
