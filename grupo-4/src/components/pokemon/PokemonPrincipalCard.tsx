e4645894c455da10d587341ad3aab296bde35b51
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2023-04-02 15:32:11 -0600

    [Global EDIT]
    Cambio de biblioteca nextui a etiquetas html con tailwind, por ahora solo hemos editado el componente pokemonPrincipalCard.txs, falta mas files

--- grupo-4/src/components/pokemon/PokemonPrincipalCard.tsx
+++ grupo-4/src/components/pokemon/PokemonPrincipalCard.tsx
@@ -11,36 +11,35 @@ interface PokemonProps {
 export const PokemonPrincipalCard: NextPage<PokemonProps> = ({ pokemon }) => {
   return (
     <Layout title={`${pokemon.name}`} data-testid={`pokemon-name`}>
-      <Grid.Container css={{ marginTop: "5px" }} gap={2}>
-        <Grid xs={12} sm={4}>
-          <Card isHoverable css={{ padding: "30px" }} data-testid={`pokemon-card`}>
-            <Card.Body>
-            <Card.Image src={pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name} width="100%" height={200} data-testid={`pokemon-card-image`} />
-             </Card.Body>
-          </Card>
-        </Grid>
+      <div >
+        <div>
+          <div>
+            <main>
+            <img src= {pokemon.sprites.other?.dream_world.front_default || "/no-image.png"} alt={pokemon.name}  />
+             </main>
+          </div>
+        </div>
 
-        <Grid xs={12} sm={8}>
-          <Card>
-            <Card.Header css={{ display: "flex", justifyContent: "space-between" }}>
-              <Text h1 transform="capitalize" data-testid={`pokemon-card-title`}>
+        <div>
+          <div>
+            <header >
+              <h1>
                 {pokemon.name}
-              </Text>
-            </Card.Header>
+              </h1>
+            </header>
 
-            <Card.Body>
-              <Text size={30}>Sprites:</Text>
-
-              <Container direction="row" display="flex" gap={0} data-testid={`pokemon-card-sprites`}>
-                <Image src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
-                <Image src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
-              </Container>
-            </Card.Body>
-          </Card>
-        </Grid>
-      </Grid.Container>
+            <main>
+              <h2>Sprites:</h2>
+              <div>
+              <img src={pokemon.sprites.front_default} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.back_default} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.front_shiny} alt={pokemon.name} width={100} height={100} />
+                <img src={pokemon.sprites.back_shiny} alt={pokemon.name} width={100} height={100} />
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
