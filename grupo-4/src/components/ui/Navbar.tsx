28cd9c7fe175fe6ceb97cf934c15d3a1a711c3fd
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-04-15 21:41:58 -0600

    [UPDATE] delete href del navbar

--- grupo-4/src/components/ui/Navbar.tsx
+++ grupo-4/src/components/ui/Navbar.tsx
@@ -4,7 +4,7 @@ import { Search } from "./searchPokemon";
 export const Navbar = () => {
   return (
     <div className=" lg:h-14 bg-blue-400 lg:grid lg:grid-cols-2 h-16 ">
-      <h1 className="lg:mt-2 ml-2 lg:text-4xl  text-blue-800 sm:text-2xl"><a href="/">Pokedex</a></h1>
+      <h1 className="lg:mt-2 ml-2 lg:text-4xl  text-blue-800 sm:text-2xl">Pokedex</h1>
       <Search  />
     </div>
   );
